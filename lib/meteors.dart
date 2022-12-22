26cf118098487d9e9cb3974f856ff3e33025ab54
dgtyPedro <pedrogcs0104@gmail.com>
2022-12-22 17:49:51 -0300

    resolvido bug 157

--- lib/meteors.dart
+++ lib/meteors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:developer' as dev;
 import 'dart:math';
+import 'dart:core';
 
 class Meteors extends StatefulWidget {
   @override
@@ -14,8 +15,10 @@ class _MeteorsState extends State<Meteors> {
   double xAxis = 0;
   double xMovement = 5;
   double yMovement = 5;
-  double screenHeight = MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.height;
-  double screenWidth = MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.width;
+  double screenHeight =
+      MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.height;
+  double screenWidth =
+      MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.width;
 
   void move() {
     Timer.periodic(Duration(milliseconds: 10), (timer2) {
@@ -23,44 +26,35 @@ class _MeteorsState extends State<Meteors> {
         yAxis = yAxis + yMovement;
         xAxis = xAxis + xMovement;
       });
-      if(yAxis > MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.height
-      || xAxis > screenWidth){
-        int genSpawn = random.nextInt(3);
-        switch (genSpawn){
+      if (yAxis > screenHeight || xAxis > screenWidth || yAxis < 0 || xAxis < 0) {
+        int genSpawn = random.nextInt(4);
+        switch (genSpawn) {
           case 0:
-            dev.log('0');
             yAxis = 0;
             xAxis = random.nextInt(screenWidth.toInt()).toDouble();
             break;
           case 1:
-            dev.log('1');
             yAxis = screenHeight;
             xAxis = random.nextInt(screenWidth.toInt()).toDouble();
             break;
           case 2:
-            dev.log('2');
             yAxis = random.nextInt(screenHeight.toInt()).toDouble();
             xAxis = 0;
             break;
           case 3:
-            dev.log('3');
             yAxis = random.nextInt(screenHeight.toInt()).toDouble();
             xAxis = screenWidth;
             break;
         }
-        if (xAxis < screenWidth/2) {
-          dev.log('4');
-          xMovement = 5;
-        }else{
-          dev.log('5');
-          xMovement = -5;
+        if (xAxis < screenWidth / 2) {
+          xMovement = random.nextInt(5).toDouble();
+        } else {
+          xMovement = random.nextInt(5).toDouble() - 6;
         }
-        if (yAxis < screenHeight/2) {
-          dev.log('6');
-          yMovement = 5;
-        }else{
-          dev.log('7');
-          yMovement = -5;
+        if (yAxis < screenHeight / 2) {
+          yMovement = random.nextInt(5).toDouble();
+        } else {
+          yMovement = random.nextInt(5).toDouble() - 6;
         }
       }
     });
@@ -78,9 +72,10 @@ class _MeteorsState extends State<Meteors> {
             child: Container(
               width: 30.0,
               height: 30.0,
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                 color: Colors.blue,
                 shape: BoxShape.circle,
+                border: Border.all(),
               ),
             ),
           ),
